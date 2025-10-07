9b699a631ec273d6e2fe38015d28036184e37506
nephriteagain <oneabove232@gmail.com>
2025-10-07 22:53:57 +0800

    chore: webpack

--- web/next.config.ts
+++ web/next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.md$/,
+      type: "asset/source",
+    });
+    return config;
+  },
   turbopack: {
     rules: {
       "*.md": {
