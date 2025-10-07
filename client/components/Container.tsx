4860d41b11e9ac74ccf75aba1189af993b26f10c
nephriteagain <oneabove232@gmail.com>
2025-10-07 22:37:26 +0800

    feat: deployment preparation

--- client/components/Container.tsx
+++ client/components/Container.tsx
@@ -1,6 +1,7 @@
 import { useAppTheme } from "@/providers/ThemeProvider";
 import { ReactNode } from "react";
 import { View, ViewProps } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 export interface ContainerProps extends ViewProps {
     children: ReactNode;
@@ -8,12 +9,14 @@ export interface ContainerProps extends ViewProps {
 
 export default function Container({ children, style, ...props }: ContainerProps) {
     const theme = useAppTheme();
+    const insets = useSafeAreaInsets();
     return (
         <View
             style={[
                 {
                     flex: 1,
                     backgroundColor: theme.colors.surfaceVariant,
+                    paddingBottom: insets.bottom,
                 },
                 style,
             ]}
