7741dda55e34d85faf266d33808d50d94b34c4bf
nephriteagain <oneabove232@gmail.com>
2025-10-11 12:09:49 +0800

    chore: remove analytics

--- web/config/firebase.ts
+++ web/config/firebase.ts
@@ -1,4 +1,3 @@
-import { getAnalytics } from "firebase/analytics";
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
@@ -15,4 +14,3 @@ const firebaseConfig = {
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
-export const analytics = getAnalytics(app);
